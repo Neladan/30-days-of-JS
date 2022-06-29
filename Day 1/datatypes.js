c55e4d27848c38427fb2c6081aa0cea79074f97f
Lionel ADANKON <lionelad006@gmail.com>
2022-06-29 21:42:42 +0100

    Day 1

---
+++ Day 1/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof leo);
+console.log(typeof bool);
+console.log(typeof undef);
+console.log(typeof vide);
---
+++ Day 1/variable.js
@@ -0,0 +1,33 @@
+let str = 'Leo';
+let bool = true;
+let undef;
+let vide = null;
+
+// decalre four variables without assigning values
+let lio;
+let ll;
+let ou;
+let oui;
+
+// decalre four variables with assigning values
+
+let oi = 5;
+let mi = 6;
+let oh = "Oulala";
+const pi = 3.14;
+
+// declare variables in multiple lines
+let firstName = 'Lionel';
+let lastName = 'ADANKON';
+let maritalStatus = 'Single';
+let country = 'Benin';
+let age = 20;
+
+// declare variables in multiple lines
+let firstNam = 'Lionel', lastNam = 'ADANKON', maritalStatu = 'Single', countr = 'Benin', ag = 20;
+
+// 
+let myAge = 25, yourAge = 30;
+
+console.log('I am ',myAge, ' years old.')
+console.log('You are ', yourAge, ' years old.')
